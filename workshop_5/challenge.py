d7745d5e97a63cf670f4191c2ca2e8f4cb7fb665
lukatavkhelidze <tavkhelidzeluka@gmail.com>
2025-10-09 20:38:36 +0400

    Add challenge solution

--- workshop_5/challenge.py
+++ workshop_5/challenge.py
@@ -6,3 +6,25 @@ when user types "done" in you input you should stop taking any new numbers and d
 2. save each number that is divisible by 7 into separate list
 3. print both list contents
 """
+
+odd_numbers = []
+numbers_divisible_by_seven = []
+
+while True:
+    user_input = input("Enter a number (type \"done\" to quit): ")
+    if user_input.lower() == "done":
+        print("Bye")
+        break
+
+    num = float(user_input)
+    if num % 2 == 1:
+        odd_numbers.append(num)
+    if num % 7 == 0 :
+        numbers_divisible_by_seven.append(num)
+
+
+odd_numbers.sort()
+numbers_divisible_by_seven.sort()
+print(f"{odd_numbers = }")
+print(f"{numbers_divisible_by_seven = }")
+
